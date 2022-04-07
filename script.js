bb1c3bbfdfbe284dc2fb579b4215e096ff5b26ca
Noah Mailloux <noah.mailloux1@gmail.com>
2022-04-07 11:49:20 -0500

    all done

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
-function game(){
+   let roundNum = 0;
+   const results = document.querySelector('#results');
+   results.setAttribute('style', 'white-space: pre;');
 
-    // randomly returns either ROCK, PAPER, or SCISSORS
+   // event listeners for buttons that play a round
+    const rockBtn = document.querySelector('#rock');
+    let playerScore = 0;
+    let computerScore = 0;
+
+    rockBtn.addEventListener('click', () => {
+        roundNum++;
+        game('ROCK')
+        
+    });
+
+    const paperBtn = document.querySelector('#paper');
+    paperBtn.addEventListener('click', () => {
+        roundNum++;
+        game('PAPER')
+        
+    });
+
+    const scissorsBtn = document.querySelector('#scissors');
+    scissorsBtn.addEventListener('click', () => {
+        roundNum++;
+        game('SCISSORS')
+        
+    });
+
+// 
+function game(playerSelection){
+        if(roundNum === 6){
+            results.textContent = "Results:";
+            playerScore = 0;
+            computerScore = 0;
+            roundNum = 1;
+        }
+
+        let computerSelection = computerPlay();
+        
+        results.textContent += `\r\n Round ${roundNum} \r\n You chose ${playerSelection} and computer chose ${computerSelection}. \r\n ${getWinner(playerSelection, computerSelection)} \r\n`    
+        results.textContent += ` The score is ${playerScore} to ${computerScore}. \r\n`
+        
+        if(roundNum === 5){
+            if(playerScore > computerScore){
+                results.textContent += '\r\n User wins the whole game!';
+            } else if (playerScore < computerScore){
+                results.textContent += '\r\n Computer wins the whole game!';
+            } else {
+                results.textContent += '\r\n The game ends in a tie!';
+            }
+        }
+    }
+
+
+    
+    // computer randomly returns either ROCK, PAPER, or SCISSORS
     function computerPlay(){
         let answer = Math.floor(Math.random() * 3);
         if (answer === 0) {
@@ -11,19 +65,8 @@ function game(){
             return "SCISSORS";
         }
     }
-    
-    // request the user to enter a non-case-sensitive string containing ROCK, PAPER, or SCISSORS
-    function getPlayerSelection(){
-        let playerSelection = (prompt("Type ROCK, PAPER, or SCISSORS", "ROCK")).toUpperCase();
-    
-        if(playerSelection !== "ROCK" && playerSelection !== "PAPER" && playerSelection !== "SCISSORS"){
-            alert("Enter in a valid response");
-            getPlayerSelection();
-        }
-        return playerSelection;
-    }
-    
-    // 
+
+    // decides if the user or computer won that round
     function getWinner (playerSelection, computerSelection){
         if (playerSelection === computerSelection) {
             return "TIE"
@@ -45,38 +88,5 @@ function game(){
         } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
             playerScore++;
             return "USER WINS"
-        } 
+        }
     }
-    
-    for (let i = 1; i<=5; i++){
-    let computerSelection = computerPlay();
-    let playerSelection = getPlayerSelection();
-
-    alert(`Round ${i}
-    You chose ${playerSelection} and computer chose ${computerSelection}.
-    ${getWinner(playerSelection, computerSelection)}`);
-    }
-}
-let playerScore = 0;
-let computerScore = 0;
-game()
-if(playerScore > computerScore){
-    alert(`The score is ${playerScore} to ${computerScore}.
-User wins!`)
-} else if (playerScore < computerScore){
-    alert(`The score is ${playerScore} to ${computerScore}.
-Computer wins!`)
-} else {
-    alert("It's a tie!")
-}
-
-
-
-
-
-
-
-// get rock/paper/scissors from user
-// computer generates rock/paper/scissors
-// compare answers and return winner
-
